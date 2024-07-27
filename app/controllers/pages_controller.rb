4948e3173086ea02d30c516076aad34c1a2f3416
Antonio Oliveira <antoniop_neto@hotmail.com>
2024-07-27 14:26:00 +0100

    recalling Rails first lesson

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+    @members= ["claire", "toni", "santi", "sara", "olivia"]
+    search = params["member"]
+    if search
+      @members = @members.select do |member|
+        member.start_with? search.downcase
+      end
+    end
+  end
+end
